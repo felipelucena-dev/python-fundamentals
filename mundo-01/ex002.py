722065c85fad52f1cc61173719407e7b8e3fb6e7
Felipe Lucena <felipevieiralucena02@gmail.com>
2026-04-27 08:46:24 -0300

    Add birth date input and confirmation message

--- mundo-01/ex002.py
+++ mundo-01/ex002.py
@@ -1,2 +1,4 @@
-nome = input('Qual é o seu nome? ')
-print(f'Olá {nome}! Prazer em te conhecer!')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print(f'Você nasceu no dia {dia} de {mes} de {ano}. Correto?')
