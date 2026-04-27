908c98fe612057dddb1e020e6f6e29b0fd473a1b
Felipe Vieira de Lucena <felipevieiralucena02@gmail.com>
2026-04-27 08:33:27 -0300

    feat: estrutura de pastas finalizada

---
+++ mundo-01/ex003.py
@@ -0,0 +1,8 @@
+# Coletando os dados e já convertendo para número inteiro
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+soma = n1 + n2
+
+# Exibindo o resultado de forma elegante
+print(f'A soma entre {n1} e {n2} é igual a {soma}!')
